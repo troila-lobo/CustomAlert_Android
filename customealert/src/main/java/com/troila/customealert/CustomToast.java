d2bc38b851d9e7ede30e02919d8459938599311f
sunjingcat <sj2012518@126.com>
2019-01-04 10:58:19 +0800

    Update CustomToast.java

--- customealert/src/main/java/com/troila/customealert/CustomToast.java
+++ customealert/src/main/java/com/troila/customealert/CustomToast.java
@@ -1,5 +1,8 @@
 package com.troila.customealert;
+import android.app.Activity;
 import android.content.Context;
+import android.widget.Toast;
+
 import com.troila.customealert.custoast.ToastUtils;
 
 public class CustomToast {
@@ -10,7 +13,11 @@ public class CustomToast {
      * @param msg
      */
     public static void showToast(Context context,String icon,String msg){
-        ToastUtils.makeText(context,msg,icon, ToastUtils.LENGTH_SHORT).show();
+        if (context instanceof Activity) {
+            ToastUtils.makeText(context, msg, icon, ToastUtils.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        }
 
     }
 
