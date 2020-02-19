254cef7c57ce5921e06b789a6ce3ffb135048d4c
xijie <xijie@troila.com>
2020-02-19 17:11:01 +0800

    修改icon为空时没有吐司

--- customealert/src/main/java/com/troila/customealert/CustomToast.java
+++ customealert/src/main/java/com/troila/customealert/CustomToast.java
@@ -1,4 +1,5 @@
 package com.troila.customealert;
+
 import android.app.Activity;
 import android.content.Context;
 import android.widget.Toast;
@@ -10,16 +11,19 @@ public class CustomToast {
 
     /**
      * 带图标和内容的Toast
+     *
      * @param icon
      * @param msg
      */
-    public static void showToast(Context context,String icon,String msg){
-    	 if(context!=null && !TextUtils.isEmpty(icon) && !TextUtils.isEmpty(msg)){
-             if (context instanceof Activity) {
-            	 ToastUtils.makeText(context, msg, icon, ToastUtils.LENGTH_SHORT).show();
-       	 }else {
-            	Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
-       	 }
+    public static void showToast(Context context, String icon, String msg) {
+        if (context != null && !TextUtils.isEmpty(icon) && !TextUtils.isEmpty(msg)) {
+            if (context instanceof Activity) {
+                ToastUtils.makeText(context, msg, icon, ToastUtils.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+            }
+        }else if(context != null && !TextUtils.isEmpty(msg)){
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
         }
     }
 
